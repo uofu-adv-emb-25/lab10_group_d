99b1e1b7555310311ee82ecb816ad22707a431b1
Emily Yang <u0835353@utah.edu>
2025-11-24 13:03:44 -0700

    Got external LED to blink on GPIO pin

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -16,30 +16,46 @@
 int count = 0;
 bool on = false;
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+//#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+//#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
-#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
+    // while (true) {
+
+        
+    //     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+    //     if (count++ % 11) on = !on;
+    //     vTaskDelay(500);
+    // }
+
+    // Select GPIO 16 (pin 21 on the Pico)
+    const uint PIN = 16;
+
+    gpio_init(PIN);               // Initialize the GPIO
+    gpio_set_dir(PIN, GPIO_OUT);  // Set as output
+    gpio_disable_pulls(PIN);      // Disable pull-up and pull-down resistors
+
+    while (1) {
+        gpio_put(PIN, 1);
+        vTaskDelay(500);
+        gpio_put(PIN, 0);
         vTaskDelay(500);
     }
 }
 
-void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
-    }
-}
+// void main_task(__unused void *params) {
+//     xTaskCreate(blink_task, "BlinkThread",
+//                 BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+//     char c;
+//     while(c = getchar()) {
+//         if (c <= 'z' && c >= 'a') putchar(c - 32);
+//         else if (c >= 'A' && c <= 'Z') putchar(c + 32);
+//         else putchar(c);
+//     }
+// }
 
 int main( void )
 {
@@ -47,8 +63,8 @@ int main( void )
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    xTaskCreate(blink_task, "BlinkThread",
+                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     vTaskStartScheduler();
     return 0;
 }
